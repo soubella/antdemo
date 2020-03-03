2662eb24a54469366a07b3c84d88c69efd95feef
said <said.oubellaa@gmail.com>
2020-03-03 09:20:48 +0100

    add smthg

--- src/Ant.java
+++ src/Ant.java
@@ -1,5 +1,6 @@
 public class Ant {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("git checkout -b feature/add_add_smthg");
     }
 }
